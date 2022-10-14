1254820b298b1806b201f4d57406fbd5d546159b
Rein <nureinumarchesano@gmail.com>
2022-10-14 14:17:49 -0400

    Class

--- src/main.py
+++ src/main.py
@@ -1,18 +1,46 @@
 import pandas as pd
 import os
 from sklearn.neighbors import KNeighborsClassifier
+import pickle
+
+MODEL_PATH = "model.pkl"
+
+class KNN:
+
+    def __init__(self, neighbors):
+        self.neighbors = neighbors
+        self.model = KNeighborsClassifier(n_neighbors=neighbors)
+
+
+    def fit(self):
+        cwd = os.getcwd()
+        data = pd.read_csv(os.path.join(cwd, "../", "dataset", "data_with_class_column.csv"))
+        X = data[['Ia', 'Ib', 'Ic', 'Va', 'Vb', 'Vc']].values
+        y = data['Class'].values
+        self.model.fit(X, y)
+
+        if not os.path.exists(MODEL_PATH):
+            with open(MODEL_PATH, "wb") as m:
+                pickle.dump(self.model, m)
+    
+    def getModel():
+        with open(MODEL_PATH, "rb") as m:
+            return pickle.load(m)
+    
+    def predict(self, X):
+        return self.model.predict(X)
+    
 
 def main():
     
-    cwd = os.getcwd()
-    data = pd.read_csv(os.path.join(cwd, "../", "dataset", "data_with_class_column.csv"))
+    if not os.path.exists(MODEL_PATH):
+        model = KNN(5)
+        model.fit()
 
-    model = KNeighborsClassifier(n_neighbors=5)
-    X = data[['Ia', 'Ib', 'Ic', 'Va', 'Vb', 'Vc']].values
-    y = data['Class'].values
-    model.fit(X, y)
+    else:
+        model = KNN.getModel()
 
-    print(model.predict([[40, -90, 70, 10, 10, 10]]))
+    print(model.predict([[-40, 9, -70, -100, 100, 100]]))
 
 
 if __name__ == "__main__":
